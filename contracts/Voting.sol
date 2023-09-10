3554976a36255638930343ecec857ffbe531cf02
Atmos <1999bag@gmail.com>
2023-09-10 17:24:41 +0300

    Added vote function

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -35,4 +35,21 @@ contract VotingContract is Ownable {
         voting.question = _question;
         voting.candidates = _candidates;
     }
+
+    function vote(uint256 _id, uint256 candidateNumber) public {
+        require(_id > 0 && _id <= votingsCount);
+
+        Voting storage voting = votings[_id];
+        //проверить на существование голосования
+        require(_id == voting.id);
+        //проверить на существование кандидата
+        require(
+            candidateNumber >= 0 && candidateNumber < voting.candidates.length,
+            "Wrong candidate number!"
+        );
+        //проверить голосовал ли уже данный человек
+        require(!voting.voters[msg.sender].voted, "You cannot vote anymore!");
+        //проверить, открыто ли голосование
+        require(voting.opened, "This voting already closed!");
+    }
 }
